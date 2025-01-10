09fbc9c1d09d604097c03ac32e80d8b091a83d96
조유민 <nationmilo@gmail.com>
2025-01-11 01:05:00 +0900

    feat: init data

---
+++ src/main/java/com/vmate/vmatebe/VmateBeApplication.java
@@ -0,0 +1,13 @@
+package com.vmate.vmatebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmateBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmateBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vmate/vmatebe/VmateBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmate.vmatebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmateBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
